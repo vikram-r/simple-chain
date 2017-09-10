7c612ef82c4c45dc84a6c6db494d1e94a64324db
Vikram Ramesh <vikram132@gmail.com>
2017-09-09 22:32:56 -0700

    Added hash function for blocks

--- src/main/scala/SimpleBlock.scala
+++ src/main/scala/SimpleBlock.scala
@@ -1,7 +1,19 @@
 
+import java.security.MessageDigest
+
 /**
   * Represents a block in the chain
-  * @param index the index of this block in the chain
-  * @param payload the payload data
+  *
+  * @param index    the index of this block in the chain
+  * @param payload  the payload data
+  * @param lastHash hash of the previous block
   */
-case class SimpleBlock(index: Int, payload: String, hash: String, lastHash: String)
+case class SimpleBlock(index: Int, payload: String, lastHash: String) {
+
+  val hash: String =
+    MessageDigest.getInstance("MD5")
+      .digest(s"$index$payload$lastHash".getBytes("UTF-8"))
+      .map("%02x".format(_))
+      .mkString
+
+}
